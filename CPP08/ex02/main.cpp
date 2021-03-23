256d919ffdf3d01e919033d1b3aab807eb03b912
AzodFR <azod.contact@gmail.com>
2021-03-24 00:51:12 +0100

    cpp08/ex02

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -5,11 +5,50 @@
 int main()
 {
 	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
+	mstack.push(5); // 5
+	mstack.push(17); // 5 17
 	std::cout << mstack.top() << std::endl;
 
-	mstack.pop();
+	mstack.pop(); // 5
 	std::cout << mstack.size() << std::endl;
-	mstack<int>::iterator;
+
+	mstack.push(3); // 5 3
+	mstack.push(5); // 5 3 5
+	mstack.push(737); // 5 3 5 737
+	mstack.push(0); // 5 3 5 737 0
+
+
+	std::cout << "Normal iteration" << std::endl;
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while(it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+
+	std::cout << "Reverse iteration" << std::endl;
+
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+
+	++rit;
+	--rit;
+
+	while(rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	std::cout << "Size of  mutant: " << mstack.size() << std::endl;
+
+	std::stack<int> s(mstack);
+	std::cout << "Size of real std::stack copied from mutant: " << s.size() << std::endl;
+	return 0;
 }
--- CPP08/ex02/mutantstack.hpp
+++ CPP08/ex02/mutantstack.hpp
@@ -8,14 +8,34 @@ class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack() : std::stack<T>(){};
-	template <class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T&>
-  	struct iterator {
-		typedef T         value_type;
-		typedef Distance  difference_type;
-		typedef Pointer   pointer;
-		typedef Reference reference;
-		typedef Category  iterator_category;
-  };
+		MutantStack(const MutantStack& obj) : std::stack<T>(obj) {};
+        virtual ~MutantStack() {};
+        MutantStack &operator=(const MutantStack &obj)
+		{
+            if (this == &obj)
+				return (*this);
+            return (*new(this) MutantStack(obj));
+        };
+		typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+        typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+		iterator begin()
+		{
+			return (std::stack<T>::c.begin()); 
+		}
+        iterator end()   
+		{
+			return (std::stack<T>::c.end());   
+		}
+        reverse_iterator rbegin()
+		{
+			return (std::stack<T>::c.rbegin());
+		}
+        reverse_iterator rend()
+		{
+			return (std::stack<T>::c.rend());
+		}
 };
 
 #endif
