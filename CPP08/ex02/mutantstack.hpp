6cf4e87d45b0a47845afa141baeb8140c29be7cf
AzodFR <40648473+AzodFR@users.noreply.github.com>
2021-03-24 00:52:44 +0100

    Update mutantstack.hpp

--- CPP08/ex02/mutantstack.hpp
+++ CPP08/ex02/mutantstack.hpp
@@ -16,11 +16,9 @@ class MutantStack : public std::stack<T>
 				return (*this);
             return (*new(this) MutantStack(obj));
         };
-		typedef typename std::stack<T>::container_type::iterator iterator;
-        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::iterator iterator;
         typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-        typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-		iterator begin()
+	iterator begin()
 		{
 			return (std::stack<T>::c.begin()); 
 		}
